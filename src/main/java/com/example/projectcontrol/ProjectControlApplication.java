e62b109817de8a83771dc91fd305fed7ecdb375e
joelmalacas <joelmalacas@gmail.com>
2026-08-27 10:04:01 +0100

    first commit

---
+++ src/main/java/com/example/projectcontrol/ProjectControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectcontrol/ProjectControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
