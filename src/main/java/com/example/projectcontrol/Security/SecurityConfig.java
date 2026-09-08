bdd661436fed329e2c1e1a53e83fb8ceeba785cf
joelmalacas <joelmalacas@gmail.com>
2026-09-08 12:06:41 +0100

    FIX SecurityFilterChain

--- src/main/java/com/example/projectcontrol/Security/SecurityConfig.java
+++ src/main/java/com/example/projectcontrol/Security/SecurityConfig.java
@@ -35,23 +35,20 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                // 1. Desativar CSRF (obrigatorio para APIs REST/JWT)
                 .csrf(AbstractHttpConfigurer::disable)
 
-                // 2. Não criar sessões HTTP (Stateless)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
 
-                // 3. Definir permissões de acesso
                 .authorizeHttpRequests(auth -> auth
-                        // Permitir explicitamente qualquer método em /api/users e /api/users/login
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users", "/api/users/login").permitAll()
-                        .requestMatchers("/api/users/login").permitAll()
                         .anyRequest().authenticated()
                 )
 
-                // 4. Inserir o filtro JWT
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
