1b288e8485ae280a9d9a720c5dfc93c1875f3d17
Joel Malacas <joelmalacas@gmail.com>
2026-09-14 11:58:07 +0100

    Add Endpoint on securityFilterChain

--- src/main/java/com/example/projectcontrol/Security/SecurityConfig.java
+++ src/main/java/com/example/projectcontrol/Security/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users", "/api/users/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/project/by-name/*/status").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
